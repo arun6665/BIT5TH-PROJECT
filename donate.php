1b6305ed6f3a6b23f305f130a8d0823c66cb888c
arun6665 <tigermhs77@gmail.com>
2020-11-29 07:32:23 +0545

    second commit

---
+++ donate.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html style="height: 706px;margin: 93px;padding: 49px;width: 1178px;">
+
+<head>
+    
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/Pricing-Table-Style-01-1.css">
+    <link rel="stylesheet" href="assets/css/Pricing-Table-Style-01.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+      <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>project1</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Barlow:300,400,500,600,700,900,900i">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,700">
+    <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
+    <link rel="stylesheet" href="assets/fonts/fontawesome5-overrides.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css">
+    <link rel="stylesheet" href="assets/css/Animate-Cards-Devices.css">
+    <link rel="stylesheet" href="assets/css/Animated-gradient-background-1.css">
+    <link rel="stylesheet" href="assets/css/Animated-gradient-background-2.css">
+    <link rel="stylesheet" href="assets/css/Animated-gradient-background.css">
+    <link rel="stylesheet" href="assets/css/Animated-Type-Heading.css">
+    <link rel="stylesheet" href="assets/css/Animation-Cards-1.css">
+    <link rel="stylesheet" href="assets/css/Animation-Cards.css">
+    <link rel="stylesheet" href="assets/css/Background-Image---Parallax---No-Text.css">
+    <link rel="stylesheet" href="assets/css/Basic-fancyBox-Gallery.css">
+    <link rel="stylesheet" href="assets/css/Contact-Form-Clean.css">
+    <link rel="stylesheet" href="assets/css/Contact-Form-v2-Modal--Full-with-Google-Map.css">
+    <link rel="stylesheet" href="assets/css/Contact-form.css">
+    <link rel="stylesheet" href="assets/css/dh-row-titile-text-image-right-1.css">
+    <link rel="stylesheet" href="assets/css/Feature-Section-MD.css">
+    <link rel="stylesheet" href="assets/css/Features.css">
+    <link rel="stylesheet" href="assets/css/Footer-Clean.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/
+    <link rel="stylesheet" href="assets/css/Modern-2-Layer-Navbar.css">
+    <link rel="stylesheet" href="assets/css/News-Cards.css">
+    <link rel="stylesheet" href="assets/css/Scroll-To-Top.css">
+    <link rel="stylesheet" href="assets/css/slider-hover.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="assets/css/WOWSlider-about-us-1.css">
+    <link rel="stylesheet" href="assets/css/WOWSlider-about-us-2.css">
+    <link rel="stylesheet" href="assets/css/WOWSlider-about-us.css">
+
+
+</head>
+
+<body>
+     
+    <ol class="breadcrumb" style="margin: 114px;background-repeat: no-repeat;background-size: cover;background-color: #cccccc;padding: 13px;">
+        <li class="breadcrumb-item"><a href="index.html"><span>Home</span></a></li>
+        <li class="breadcrumb-item"><a href="donate.php"><span>Donate Now</span></a></li>
+    </ol>
+   
+    <div class="row">
+        <div class="col-sm-4 col-md-4">
+            <div class="pricingTable purple">
+                <div class="pricingTable-header">
+                    <h3>$DANGAGRUM</h3><span></span></div>
+                <div class="pricing-plans"><span class="price-value"><span>CASH</span></span><span class="month">DONATE</span></div>
+                <div class="pricingContent">
+                    <ul></ul>
+                </div>
+                <div class="pricingTable-sign-up"><a class="btn btn-block" href="cashdonate.php">PROCEED</a></div>
+            </div>
+        </div>
+        <div class="col-sm-4 col-md-4">
+            <div class="pricingTable green">
+                <div class="pricingTable-header">
+                    <h3>$DANGARUM</h3><span></span></div>
+                <div class="pricing-plans"><span class="price-value"><span>CLOTHES</span></span><span class="month">DONATE</span></div>
+                <div class="pricingContent">
+                    <ul></ul>
+                </div>
+                <div class="pricingTable-sign-up"><a class="btn btn-block" href="detaildonor.php">PROCEED</a></div>
+            </div>
+        </div>
+        <div class="col-sm-4 col-md-4">
+            <div class="pricingTable yellow">
+                <div class="pricingTable-header">
+                    <h3>$DANGARUM</h3><span></span></div>
+                <div class="pricing-plans"><span class="price-value"></i><span>STATIONARY</span></span><span class="month">DONATE</span></div>
+                <div class="pricingContent">
+                    <ul></ul>
+                </div>
+                <div class="pricingTable-sign-up"><a class="btn btn-block" href="detaildonor.php">PROCEED</a></div>
+            </div>
+        </div>
+    </div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+   <nav class="navbar navbar-light navbar-expand-md fixed-top bg-light float-right" style="height: 110px;background-color: #1ad6ef;">
+        <div class="container-fluid"><a class="navbar-brand text-center jm-logo" href="#" style="background-image: url(&quot;assets/img/logo7_29_185350.png&quot;);padding: 10px;margin: -70px;height: 245px;background-size: contain;background-repeat: no-repeat;background-position: center;width: 295px;font-size: 38px;"></a>
+            <button
+                data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse text-secondary" id="navcol-1">
+                    <ul class="nav navbar-nav mx-auto">
+                        <li class="nav-item" role="presentation"><a class="nav-link active" href="aboutus.html" style="background-color: rgba(255,255,255,0.2);color: #000000;">About The Foundation</a></li>
+                        <li class="nav-item" role="presentation"><a class="nav-link" href="contact.html" style="background-color: rgba(255,255,255,0.2);color: #000000;">Contact</a></li>
+                        <li class="nav-item" role="presentation"><a class="nav-link active" href="#" style="background-color: rgba(255,255,255,0.2);color: #040404;">Gallery</a></li>
+                        <li class="nav-item" role="presentation"></li>
+                    </ul>
+                    <ul class="nav navbar-nav ml-auto">
+                        <li class="nav-item" role="presentation"></li>
+                        <li class="nav-item" role="presentation"><a class="nav-link active" href="#" style="width: 117px;height: 40px;color: #000000;"><i class="fas fa-sign-in-alt" style="width: 10px;height: 6px;margin: 6px;"></i>&nbsp;Join us</a></li>
+                    </ul>
+                </div>
+        </div>
+
+    </nav>
+    
+    <div class="footer-clean"  margin: 0px;">
+        <footer style="background-image: url(&quot;assets/img/dangg.png&quot;);background-repeat: no-repeat;background-size: cover;padding: 78px;height: 230px;margin: -1px;">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-sm-4 col-md-3 item">
+                        <h3>About</h3>
+                        <ul>
+                            <li><a href="#">Company</a></li>
+                            <li><a href="#">Team</a></li>
+                            <li><a href="#">FAQ</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-4 col-md-3 item">
+                        <h3>Careers</h3>
+                        <ul>
+                            <li><a href="#">Job openings</a></li>
+                            <li><a href="#">Employee success</a></li>
+                            <li><a href="#">Benefits</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-3 item social"><a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-instagram"></i></a>
+                        <p class="copyright">Company Name © 2017</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+</body>
+
+
+</html>
---
+++ signup.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>project1</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Barlow:300,400,500,600,700,900,900i">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,700">
+    <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
+    <link rel="stylesheet" href="assets/fonts/fontawesome5-overrides.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css">
+    </head>
+    <body>
+
+<form class="text-center border border-light p-5" action="#!">
+
+    <p class="h4 mb-4">Sign up</p>
+
+    <div class="form-row mb-4">
+        <div class="col">
+            <!-- First name -->
+            <input type="text" id="defaultRegisterFormFirstName" class="form-control" placeholder="First name">
+        </div>
+        <div class="col">
+            <!-- Last name -->
+            <input type="text" id="defaultRegisterFormLastName" class="form-control" placeholder="Last name">
+        </div>
+    </div>
+
+    <!-- E-mail -->
+    <input type="email" id="defaultRegisterFormEmail" class="form-control mb-4" placeholder="E-mail">
+
+    <!-- Password -->
+    <input type="password" id="defaultRegisterFormPassword" class="form-control" placeholder="Password" aria-describedby="defaultRegisterFormPasswordHelpBlock">
+    <small id="defaultRegisterFormPasswordHelpBlock" class="form-text text-muted mb-4">
+        At least 8 characters and 1 digit
+    </small>
+
+    <!-- Phone number -->
+    <input type="text" id="defaultRegisterPhonePassword" class="form-control" placeholder="Phone number" aria-describedby="defaultRegisterFormPhoneHelpBlock">
+    <small id="defaultRegisterFormPhoneHelpBlock" class="form-text text-muted mb-4">
+        Optional - for two step authentication
+    </small>
+
+    <!-- Newsletter -->
+    <div class="custom-control custom-checkbox">
+        <input type="checkbox" class="custom-control-input" id="defaultRegisterFormNewsletter">
+        <label class="custom-control-label" for="defaultRegisterFormNewsletter">Subscribe to our newsletter</label>
+    </div>
+
+    <!-- Sign up button -->
+    <button class="btn btn-info my-4 btn-block" type="submit">Sign in</button>
+
+    <!-- Social register -->
+    <p>or sign up with:</p>
+
+    <a href="#" class="mx-2" role="button"><i class="fab fa-facebook-f light-blue-text"></i></a>
+    <a href="#" class="mx-2" role="button"><i class="fab fa-twitter light-blue-text"></i></a>
+    <a href="#" class="mx-2" role="button"><i class="fab fa-linkedin-in light-blue-text"></i></a>
+    <a href="#" class="mx-2" role="button"><i class="fab fa-github light-blue-text"></i></a>
+
+    <hr>
+
+    <!-- Terms of service -->
+    <p>By clicking
+        <em>Sign up</em> you agree to our
+        <a href="redirect.php" target="_blank">terms of service</a>
+
+</form>
+<!-- Default form register -->
+</p>
+</form>
+</body>
+</html>
